b4b0f3bc13051b62e44b6537059b719302a97e86
Alex Prokop <alex.prokop@gmail.com>
2013-11-15 21:40:27 -0800

    Adds styles and transitions, looks pretty weird but not too bad

---
+++ keys.js
@@ -0,0 +1,6 @@
+var facebook = "cf5bf14d9607347dcb7f5fe9dee2dc6c";
+var languageDetection = "120bc4aefee120368bfefc70bc0a79ba";
+var wordReference = "6ef91";
+module.exports.facebook = facebook;
+module.exports.languageDetection = languageDetection;
+module.exports.wordReference = wordReference;

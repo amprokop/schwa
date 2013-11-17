eb30d49ebd2cbd1a51aea7463e020cd026af85c6
Alex Prokop <alex.prokop@gmail.com>
2013-11-16 18:51:42 -0800

    removes deleted files

--- remindr/selection.js
+++
@@ -1,9 +0,0 @@
-// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-//   if (request.method == "getSelection"){
-//       sendResponse({data: window.getSelection().toString()});
-//     console.log('response sent');
-//   else 
-//     sendResponse({}); // snub them.
-// });
-
-var fun = 'this';
